04e68eda40a4a296f085e8cabb1205279b0f4ad3
Jorick Caberio <circuitfreakcoder@gmail.com>
2013-06-03 12:19:01 +0800

    Create titlecase.cpp

---
+++ titlecase.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string title_case(string s)
+{
+    for(unsigned int i=0; i<s.length(); i++)
+    {
+        if((i==0||s[i-1]== ' ') && (s[i] >= 'a' && s[i] <= 'z'))
+            s[i] = s[i]-('a'-'A');
+    }
+    return s;
+
+}
+
+int main()
+{
+    cout << "Enter string:" << endl;
+    string x;
+    getline(cin, x);
+    cout<<"Title Case: " << title_case(x);
+    
+    return 0;
+}
